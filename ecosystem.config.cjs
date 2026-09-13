45ead2a086956ea559a7f4ba940cdbe8e640492e
Dev-DanielOffc <danielplacencio15@gmail.com>
2026-09-13 10:24:30 -0400

    Crear ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'messageschat',
+      script: 'src/server.js',
+      cwd: __dirname,
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      node_args: '--max-old-space-size=450',
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      time: true
+    }
+  ]
+};
